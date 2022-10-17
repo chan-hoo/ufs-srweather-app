c0f341c7eeaab0b05d18a99234a091a784743918
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2022-10-17 00:54:25 +0000

    integrate aqm.rc templates using jinja2

--- scripts/exregional_run_fcst.sh
+++ scripts/exregional_run_fcst.sh
@@ -441,9 +441,7 @@ if [ "${CPL_AQM}" = "TRUE" ]; then
 #-----------------------------------------------------------------------
 #
   init_concentrations="false"
-  cycl_hrs_1st2d=$( printf "%02d" "${CYCL_HRS[0]}" )
-  date_first_cycl_ymdh="${DATE_FIRST_CYCL:0:8}${cycl_hrs_1st2d}"
-  if [ "${COLDSTART}" = "TRUE" ] && [ "${PDY}${cyc}" = "${date_first_cycl_ymdh}" ]; then
+  if [ "${COLDSTART}" = "TRUE" ] && [ "${PDY}${cyc}" = "${DATE_FIRST_CYCL:0:10}" ]; then
     init_concentrations="true"
   fi
 #
