27e7c1d8c875a3031069ae338ca8015e5a8e4922
chan-hoo <chan-hoo.jeon@noaa.gov>
2023-06-18 21:34:53 +0000

    update ecflow manager script

--- scripts/exregional_ecflow_manager.sh
+++ scripts/exregional_ecflow_manager.sh
@@ -45,15 +45,15 @@ In directory:     \"${scrfunc_dir}\"
 This is the ex-script for AQM-MANAGER.
 ========================================================================"
 
-ecflow_sid=prod_aqm
-
 #-----------------------------------------------------------------------
 #
 # Set variables used in the script
 #
 #-----------------------------------------------------------------------
 
-CDATE=${PDY}${cyc}
+ecflow_sid="${envir}_${NET}"
+
+CDATE="${PDY}${cyc}"
 GDATE=$($NDATE -06 $CDATE)
 gPDY=$(echo $GDATE | cut -c1-8)
 gcyc=$(echo $GDATE | cut -c9-10)
@@ -61,24 +61,28 @@ NEXTDATE=$($NDATE +06 $CDATE)
 NEXTPDY=$(echo $NEXTDATE | cut -c1-8)
 NEXTcyc=$(echo $NEXTDATE | cut -c9-10)
 
-CURRENTDAYPDY=$(ecflow_client --query variable /${ecflow_sid}/primary/${gcyc}:PDY)00
+CURRENTDAYPDY=$(ecflow_client --query variable /${ecflow_sid}/${NET}/${model_ver}/${gcyc}:PDY)00
 NEXTDAYPDY_CDATE=$($NDATE +24 $CURRENTDAYPDY)
 NEXTDAYPDY=$(echo $NEXTDAYPDY_CDATE | cut -c1-8)
 
-if [ $RUN_ENVIR = "nco" ] && [ "${DO_REAL_TIME}" = "TRUE" ]; then
-  gefs_check=$(compath.py ${envir}/gefs/${gefs_ver})/gefs.${NEXTPDY}/${NEXTcyc}/chem/sfcsig/geaer.t${NEXTcyc}z.atmf078.nemsio
-  gfs_check=$(compath.py ${envir}/gfs/${gfs_ver})/gfs.${NEXTPDY}/${NEXTcyc}/atmos/gfs.t${NEXTcyc}z.sfcf078.nc
+gefs_check=false
+gfs_check=false
+if [ "${CPL_AQM}" = "TRUE" ] && [ "${DO_REAL_TIME}" = "TRUE" ]; then
+  gefs_fp=$(compath.py ${envir}/gefs/${gefs_ver})/gefs.${NEXTPDY}/${NEXTcyc}/chem/sfcsig/geaer.t${NEXTcyc}z.atmf078.nemsio
+  gfs_fp=$(compath.py ${envir}/gfs/${gfs_ver})/gfs.${NEXTPDY}/${NEXTcyc}/atmos/gfs.t${NEXTcyc}z.sfcf078.nc
+
+  [[ -f ${gefs_fp} ]] && gefs_check=true
+  [[ -f ${gfs_fp} ]] && gfs_check=true
 else
   gefs_check=true
   gfs_check=true
 fi
 
-found_required_file_for_next_cycle=NO
-fcst_job_completed=NO
+found_required_file_for_next_cycle=false
+fcst_job_completed=false
+
+# ecFlow flags
 release_next_cycle=NO
-previous_cycle_run_completed=NO
-cleanup_job_done=NO
-previous_cycle_aqm_manager_complete=NO
 
 h_try=1
 while [ $h_try -lt 70 ]; do
@@ -89,11 +93,11 @@ while [ $h_try -lt 70 ]; do
   #
   #-----------------------------------------------------------------------
 
-  if [ ${found_required_file_for_next_cycle} = "NO" ]; then
+  if [ ! ${found_required_file_for_next_cycle} ]; then
     forecast_RESTART_file=$(ls ${COMIN}/RESTART/*coupler.res|wc -l)
-    if [[ ${forecast_RESTART_file} -ge 1 && -f ${gefs_check} && -f ${gfs_check} ]]; then
+    if [ ${forecast_RESTART_file} -ge 1 ] && [ ${gefs_check} ] && [ ${gfs_check} ]; then
       ecflow_client --event release_next_cycle
-      found_required_file_for_next_cycle=YES
+      found_required_file_for_next_cycle=true
     fi
   fi
 
@@ -103,19 +107,19 @@ while [ $h_try -lt 70 ]; do
   #
   #-----------------------------------------------------------------------
 
-  if [ ${fcst_job_completed} = "NO" ]; then
-    P_state=$(ecflow_client --query state /${ecflow_sid}/primary/${gcyc})
-    e_state=$(ecflow_client --query state /${ecflow_sid}/primary/${cyc}/aqm/v1.0/forecast/jforecast)
-    if [[ ${e_state} = "complete" && ${P_state} = "complete" ]]; then
+  if [ ! ${fcst_job_completed} ]; then
+    P_state=$(ecflow_client --query state /${ecflow_sid}/${NET}/${model_ver}/${gcyc})
+    e_state=$(ecflow_client --query state /${ecflow_sid}/${NET}/${model_ver}/${cyc}/coldstart/jrun_fcst)
+    if [ ${e_state} = "complete" ] && [ ${P_state} = "complete" ]; then
       ecflow_client --event requeue_cycle
-      ecflow_client --alter change variable PDY $NEXTDAYPDY /${ecflow_sid}/primary/${gcyc}
-      ecflow_client --requeue force /${ecflow_sid}/primary/${gcyc}/aqm
-      fcst_job_completed=YES
+      ecflow_client --alter change variable PDY $NEXTDAYPDY /${ecflow_sid}/{NET}/${model_ver}/${gcyc}
+      ecflow_client --requeue force /${ecflow_sid}/${NET}/${model_ver}/${gcyc}
+      fcst_job_completed=true
     fi
   fi 
 
   h_try=$((h_try+1))
-  if [ ${found_required_file_for_next_cycle} = "YES" -a ${fcst_job_completed} = "YES" ]; then
+  if [ ${found_required_file_for_next_cycle} ] && [ ${fcst_job_completed} ]; then
     h_try=99
   else
     sleep 300
