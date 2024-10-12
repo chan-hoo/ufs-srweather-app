afc0a13d06ceb6370bd4574dde1e4e8757b0bc34
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2024-10-12 16:44:28 +0000

    fix issue on upp_post

--- modulefiles/tasks/hera/upp_post.local.lua
+++ modulefiles/tasks/hera/upp_post.local.lua
@@ -6,6 +6,7 @@ load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
 load(pathJoin("hdf5", hdf5_ver))
 load(pathJoin("netcdf-c", netcdf_c_ver))
 load(pathJoin("netcdf-fortran", netcdf_fortran_ver))
+load(pathJoin("wgrib2", wgrib2_ver))
 load(pathJoin("prod_util", prod_util_ver))
 
 load(pathJoin("py-numpy", py_numpy_ver))
